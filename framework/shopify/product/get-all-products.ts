79203977eae58ef5803bc85bcccd26b2b26e01a6
Matan Shaviro <matanshaviro@Matans-MBP-3-2.lan>
2022-08-30 17:49:58 +0300

    Init client and server

---
+++ framework/shopify/product/get-all-products.ts
@@ -0,0 +1,20 @@
+const fetchApi = async () => {
+  const url = "https://jsonplaceholder.typicode.com/todos";
+
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await res.json();
+  return { data };
+};
+
+const getAllProducts = async (): Promise<any[]> => {
+  const products = await fetchApi();
+  return products.data;
+};
+
+export default getAllProducts;
