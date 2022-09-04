427be9931de5c7501955184b1a615bf798cc6590
Matan Shaviro <matanshaviro@Matans-MBP-3-2.lan>
2022-09-04 18:23:25 +0300

    Implement fetch products query

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,11 +1,18 @@
-const fetchApi = async () => {
-  const url = "https://jsonplaceholder.typicode.com/todos";
+import getAllProductsQuery from "../utils/queries/get-all-products";
+
+type FetchParams = {
+  query: string;
+};
+
+const fetchApi = async ({ query }: FetchParams) => {
+  const url = "http://localhost:4000/graphql";
 
   const res = await fetch(url, {
-    method: "GET",
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
+    body: JSON.stringify({ query }),
   });
 
   const data = await res.json();
@@ -13,7 +20,7 @@ const fetchApi = async () => {
 };
 
 const getAllProducts = async (): Promise<any[]> => {
-  const products = await fetchApi();
+  const products = await fetchApi({ query: getAllProductsQuery });
   return products.data;
 };
 
