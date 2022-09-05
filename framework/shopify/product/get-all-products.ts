8c04d4c8fbc7fc1b71e78fcb5c6f00d73e83c850
Matan Shaviro <matanshaviro@Matans-MBP-3-2.lan>
2022-09-05 20:12:59 +0300

    Normalize product

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,5 +1,6 @@
 import fetchApi from "../utils/fetch-api";
 import getAllProductsQuery from "../utils/queries/get-all-products";
+import { normalizeProduct } from "../utils/normalize";
 import { ProductConnection } from "../schema";
 
 type ReturnType = {
@@ -10,9 +11,8 @@ const getAllProducts = async (): Promise<any> => {
   const { data } = await fetchApi<ReturnType>({ query: getAllProductsQuery });
 
   const products =
-    data.products.edges.map(({ node: product }) => {
-      return product;
-    }) ?? [];
+    data.products.edges.map(({ node: product }) => normalizeProduct(product)) ??
+    [];
 
   return products;
 };
