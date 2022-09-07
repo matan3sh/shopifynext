9bf2709ee01322487adf54aceb241d1f403c9c31
Matan Shaviro <matanshaviro@Matans-MBP-3-2.lan>
2022-09-07 11:41:48 +0300

    ts config specific setup

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 const merge = require("deepmerge");
 
 function withFrameworkConfig(defaultConfig = {}) {
@@ -10,6 +11,14 @@ function withFrameworkConfig(defaultConfig = {}) {
     "next.config"
   ));
   const config = merge(defaultConfig, frameworkNextConfig);
+
+  const tsPath = path.join(process.cwd(), "tsconfig.json");
+  const tsConfig = require(tsPath);
+
+  tsConfig.compilerOptions.paths["@framework"] = [`framework/${framework}`];
+  tsConfig.compilerOptions.paths["@framework/*"] = [`framework/${framework}/*`];
+
+  fs.writeFileSync(tsPath, JSON.stringify(tsConfig, null, 2));
 
   return config;
 }
