535342455fad341a72892794d8e446d07e40603d
Matan Shaviro <matanshaviro@Matans-MBP-3-2.lan>
2022-09-13 16:56:09 +0300

    Implement tailwind variables

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -5,9 +5,7 @@ import styles from "./Layout.module.css";
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className={styles.root}>
-      <main style={{ color: "var(--primary)" }} className="fit">
-        {children}
-      </main>
+      <main className="fit">{children}</main>
     </div>
   );
 };
