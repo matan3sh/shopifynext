53e89f83387eb4809c2e6ebb41e8a0b73be02026
Matan Shaviro <matanshaviro@Matans-MBP-3.lan>
2022-09-14 19:07:27 +0300

    Implement Product info

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,15 +1,35 @@
 import { FC } from "react";
+import Link from "next/link";
+import Image from "next/image";
 import { Product } from "@common/types/product";
 
 interface IProps {
   product: Product;
 }
 
+const placeholderImage = "/product-image-placeholder.svg";
+
 const ProductCard: FC<IProps> = ({ product }) => {
   return (
-    <div>
-      <h3>{product.name}</h3>
-    </div>
+    <Link href={`/product/${product.slug}`}>
+      <a>
+        <div>
+          <h3>{product.name}</h3>
+          <span>14 $</span>
+        </div>
+
+        {product.images && (
+          <Image
+            alt={product.name ?? "Product Image"}
+            src={placeholderImage}
+            height={540}
+            width={540}
+            quality="85"
+            layout="responsive"
+          />
+        )}
+      </a>
+    </Link>
   );
 };
 
