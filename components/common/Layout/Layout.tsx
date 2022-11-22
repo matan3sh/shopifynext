1c5fc2b8a0a75ba4112bc93c3a407b0afdfdc3d6
Matan Shaviro <matan3sh@gmail.com>
2022-11-22 18:23:23 +0200

    Prepare Navbar component

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,11 +1,12 @@
 import { FC, PropsWithChildren } from "react";
-import { Footer } from "@components/common";
+import { Footer, Navbar } from "@components/common";
 
 import styles from "./Layout.module.css";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className={styles.root}>
+      <Navbar />
       <main className="fit">{children}</main>
       <Footer />
     </div>
