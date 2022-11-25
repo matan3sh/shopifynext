10b1c0b1af3162152c0db61d781e096bef37fb23
Matan Shaviro <matan3sh@gmail.com>
2022-11-25 20:05:13 +0200

    Implement wishlist page, cart and sidebar components

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,5 +1,7 @@
 import { FC, PropsWithChildren } from "react";
 import { Footer, Navbar } from "@components/common";
+import { Sidebar } from "@components/ui";
+import { CartSidebar } from "@components/cart";
 
 import styles from "./Layout.module.css";
 
@@ -7,6 +9,9 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className={styles.root}>
       <Navbar />
+      <Sidebar>
+        <CartSidebar />
+      </Sidebar>
       <main className="fit">{children}</main>
       <Footer />
     </div>
