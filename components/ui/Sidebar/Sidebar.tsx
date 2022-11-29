934d75be2b032122b0945defde6fce6fb1da753f
Matan Shaviro <matan3sh@gmail.com>
2022-11-29 20:50:18 +0200

    Disable body scroll when cart sidebar is open

--- components/ui/Sidebar/Sidebar.tsx
+++ components/ui/Sidebar/Sidebar.tsx
@@ -1,4 +1,9 @@
-import { FC, PropsWithChildren } from "react";
+import { FC, PropsWithChildren, useEffect, useRef } from "react";
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from "body-scroll-lock";
 
 interface Props extends PropsWithChildren {
   isOpen: boolean;
@@ -6,10 +11,27 @@ interface Props extends PropsWithChildren {
 }
 
 const Sidebar: FC<Props> = ({ children, isOpen, onClose }) => {
+  const ref = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    if (ref.current) {
+      console.log(ref.current);
+      if (isOpen) {
+        disableBodyScroll(ref.current);
+      } else {
+        enableBodyScroll(ref.current);
+      }
+    }
+
+    return () => {
+      clearAllBodyScrollLocks();
+    };
+  }, [isOpen]);
+
   return (
     <>
       {isOpen ? (
-        <div className="fixed inset-0 overflow-hidden h-full z-50">
+        <div ref={ref} className="fixed inset-0 overflow-hidden h-full z-50">
           <div className="absolute inset-0 overflow-hidden">
             <div
               onClick={onClose}
