2550fafe423e01560b49b133d49e9ab252e73dbb
Matan Shaviro <matan3sh@gmail.com>
2022-12-06 15:02:08 +0200

    Getting the products from server, Implement get static paths query

--- framework/shopify/product/get-all-products-paths.ts
+++ framework/shopify/product/get-all-products-paths.ts
@@ -1,18 +1,25 @@
 import { ApiConfig } from "@common/types/api";
 import { Product } from "@common/types/product";
+import { ProductConnection } from "@framework/schema";
+import getAllProductsPathsQuery from "@framework/utils/queries/get-all-products-paths";
 
 type ReturnType = {
   products: Pick<Product, "slug">[];
 };
 
 const getAllProductsPaths = async (config: ApiConfig): Promise<ReturnType> => {
-  return {
-    products: [
-      { slug: "cool-hat" },
-      { slug: "t-shirt" },
-      { slug: "lightweight-jacket" },
-    ],
-  };
+  const { data } = await config.fetch<{ products: ProductConnection }>({
+    query: getAllProductsPathsQuery,
+    url: config.apiUrl,
+  });
+
+  const products = data.products.edges.map(({ node: { handle } }) => {
+    return {
+      slug: handle,
+    };
+  });
+
+  return { products };
 };
 
 export default getAllProductsPaths;
