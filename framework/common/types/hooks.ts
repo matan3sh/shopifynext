203f58697e495ab50dd6bc89d1f3f3424d7a5bdd
Matan Shaviro <matan3sh@gmail.com>
2023-01-12 18:26:51 +0200

    Adding MutationHook types

---
+++ framework/common/types/hooks.ts
@@ -0,0 +1,8 @@
+export type MutationHookContext = {
+  fetch: (input: any) => any;
+};
+
+export type MutationHook = {
+  fetcher: (input: any) => any;
+  useHook(context: MutationHookContext): (input: any) => any;
+};
--- framework/shopify/cart/use-add-item.ts
+++ framework/shopify/cart/use-add-item.ts
@@ -1,8 +1,9 @@
 import { useAddItem } from "@common/cart";
+import { MutationHook } from "@common/types/hooks";
 
 export default useAddItem;
 
-export const handler = {
+export const handler: MutationHook = {
   fetcher: (input: any) => {
     return JSON.stringify(input) + "_MODIFIED";
   },
